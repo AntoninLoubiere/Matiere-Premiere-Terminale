562850b17e14880818c572cbe21b0f88cd2b6f1e
AntoninLoubiere <antonin.loubiere@laposte.net>
2019-01-13 15:10:34 +0100

    Add subject

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-let listSubject = ["Math", "Physique"];
+let listSubject = ['Art', 'Ecologie, agronomie et territoires', 'Histoire geographie', 
+'Humanite, litterature et philosophie', 'Langues et literatures etrangere', 'Mathematiques', 
+'Numerique et sciences informatique', 'SVT', 'Sciences de l\'ingenieur', 
+'Sciences economique et sociales', 'Physique chimie'];
 
 function createCheckboxInSubjectForm() {
 	var form = document.getElementById("chooseSubjectForm");
 
 	for (let i = 0; i < listSubject.length; i++) {
 		let subject = listSubject[i]
-		console.log(subject);
+
+		let cb = document.createElement('input');
+		cb.type = 'checkbox';
+		cb.name = subject;
+		cb.id = "idCheckbox" + subject;
+		form.appendChild(cb);
+
+		let label = document.createElement('label')
+		label.htmlFor = "idLabel" + subject;
+		label.appendChild(document.createTextNode(subject));
+		form.appendChild(label);
 	}
 }
 
